30a539cc651239a739d646305c60a8ebee97f05b
Konstantin060290 <73125531+Konstantin060290@users.noreply.github.com>
2020-11-04 14:54:23 +0300

    Add files via upload

---
+++ Lab06-4/Triangle.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab06_4
+{
+
+    class Triangle
+    {
+        private double a;
+        public double Prop1
+        {
+            get
+            {
+                return a;
+            }
+
+            set
+            {
+                a = value;
+            }
+        }
+        private double b;
+        public double Prop2
+        {
+            get
+            {
+                return b;
+            }
+
+            set
+            {
+                b = value;
+            }
+        }
+
+        private double c;
+
+        public double Prop3
+        {
+            get
+            {
+                return c;
+            }
+
+            set
+            {
+                c = value;
+            }
+        }
+    }
+}
