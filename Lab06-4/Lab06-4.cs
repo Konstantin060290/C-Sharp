753a78e135a819081089c9c1ea706d69720e880d
Konstantin060290 <73125531+Konstantin060290@users.noreply.github.com>
2020-11-04 15:33:17 +0300

    Add files via upload

---
+++ Lab06-4/Lab06-4.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab06_4
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Triangle Op1 = new Triangle();
+            Console.WriteLine("Введите 3 стороны треугольника:");
+            Op1.Initialisation();
+            Op1.Check();
+            Op1.Screen();
+            Console.WriteLine("Периметр треугольника равен:");
+            Op1.Perimeter();
+            Console.WriteLine("Площадь треугольника равна:");
+            Op1.Area();
+
+        }
+    }
+}
+
+
+  
+
---
+++ Lab06-4/Triangle.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab06_4
+{
+    class Triangle 
+    {
+        private double a;
+        public double Prop1
+        {
+            get
+            {
+                return a;
+            }
+
+            set
+            {
+                a = value;
+            }
+        }
+        private double b;
+        public double Prop2
+        {
+            get
+            {
+                return b;
+            }
+
+            set
+            {
+                b = value;
+            }
+        }
+
+        private double c;
+
+        public double Prop3
+        {
+            get
+            {
+                return c;
+            }
+
+            set
+            {
+                c = value;
+            }
+        }
+        
+        
+        public void Initialisation()
+        {
+            
+            Prop1 = double.Parse(Console.ReadLine());
+            Prop2 = double.Parse(Console.ReadLine());
+            Prop3 = double.Parse(Console.ReadLine());
+        }
+
+        public void Perimeter()
+        {
+            
+            double p = Prop1 + Prop2 + Prop3;
+            Console.WriteLine(p);
+            
+        }
+
+        public void Area()
+        {
+            
+            double p1;
+            p1 = (Prop1 + Prop2 + Prop3) / 2;
+            double s;
+            s = Math.Sqrt(p1 * (p1 - Prop1) * (p1 - Prop2) * (p1 - Prop3));
+            Console.WriteLine(s);
+        }
+
+        public void Screen()
+        {
+
+
+            Console.WriteLine("Стороны треугольника a={0}, b={1}, c={2}", Prop1, Prop2, Prop3);
+        }
+
+        public void Check()
+        {
+
+            if ((Prop1 + Prop2) >Prop3 && (Prop1 +Prop3) > Prop2 && (Prop2 + Prop3) > Prop1)
+            {
+                Console.WriteLine("Треугольник существует");
+
+            }
+            else
+            {
+                Console.WriteLine("Треугольник не существует");
+                System.Environment.Exit(1);
+            }
+        }
+    }
+}
