27fc0de1bae7372e04b80f140777906983f20e9d
Konstantin060290 <73125531+Konstantin060290@users.noreply.github.com>
2020-11-05 22:25:52 +0300

    Add files via upload

---
+++ Lab08-4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab08_4
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Triangle Op2 = new Triangle("Со вводом");
+            Console.WriteLine("Введите сторону треугольника:");
+            Op2.Initialisation("Равносторонний");
+            Op2.Check();
+            Op2.Screen();
+            Console.WriteLine("Периметр треугольника равен:");
+            Op2.Perimeter();
+            Console.WriteLine("Площадь треугольника равна:");
+            Op2.Area();
+
+
+            Triangle Op1 = new Triangle();
+            //Console.WriteLine("Введите 3 стороны треугольника:");
+            //Op1.Initialisation();
+            Op1.Check();
+            Op1.Screen();
+            Console.WriteLine("Периметр треугольника равен:");
+            Op1.Perimeter();
+            Console.WriteLine("Площадь треугольника равна:");
+            Op1.Area();
+            
+          
+        }
+    }
+}
---
+++ Lab08-4/Triangle.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab08_4
+{
+    class Triangle
+    {
+        private double a;
+        //public double Prop1
+        //{
+        //    get
+        //    {
+        //        return a;
+        //    }
+
+        //    set
+        //    {
+        //        a = value;
+        //    }
+        //}
+        private double b;
+        //public double Prop2
+        //{
+        //    get
+        //    {
+        //        return b;
+        //    }
+
+        //    set
+        //    {
+        //        b = value;
+        //    }
+        //}
+
+        private double c;
+
+        //public double Prop3
+        //{
+        //    get
+        //    {
+        //        return c;
+        //    }
+
+        //    set
+        //    {
+        //        c = value;
+        //    }
+        //}
+        public Triangle()
+        {
+            a = 8;
+            b = 10;
+            c = 7;
+        }
+
+        public Triangle(string eqtr)
+        {
+           
+        }
+
+
+        public void Initialisation(string eqtr2)
+        {
+
+            a = double.Parse(Console.ReadLine());
+            b = a;
+            c = a;
+            //b = double.Parse(Console.ReadLine());
+            //c = double.Parse(Console.ReadLine());
+        }
+
+        public void Perimeter()
+        {
+
+            double p = a + b + c;
+            Console.WriteLine(p);
+
+        }
+
+        public void Area()
+        {
+
+            double p1;
+            p1 = (a + b + c) / 2;
+            double s;
+            s = Math.Sqrt(p1 * (p1 - a) * (p1 - b) * (p1 - c));
+            Console.WriteLine(s);
+        }
+
+        public void Screen()
+        {
+
+
+            Console.WriteLine("Стороны треугольника a={0}, b={1}, c={2}", a, b, c);
+
+        }
+
+     
+            public void Check()
+        {
+
+            if ((a + b) > c && (a + c) > b && (b + c) > a)
+            {
+                Console.WriteLine("Треугольник существует");
+
+            }
+            else
+            {
+                Console.WriteLine("Треугольник не существует");
+                System.Environment.Exit(1);
+            }
+        }
+    }
+}
