800cb54ab4bd0cbe1a28b106c8fe30865444a707
Konstantin060290 <73125531+Konstantin060290@users.noreply.github.com>
2020-11-06 23:35:32 +0300

    Add files via upload

---
+++ Lab07-6/Book.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab07_6
+{
+    public interface IComparable
+    {
+        int CompareTo(object obj);
+
+    }
+
+    
+    class Book: IComparable
+    {
+
+        private string author;
+        public string Pr1
+        {
+            get
+            {
+                return author;
+            }
+
+            set
+            {
+               author = value;
+            }
+        }
+        private string title;
+        public string Pr2
+        {
+            get
+            {
+                return title;
+            }
+
+            set
+            {
+                title = value;
+            }
+        }
+        private int year;
+        public int Pr3
+        {
+            get
+            {
+                return year;
+            }
+
+            set
+            {
+               year = value;
+            }
+        }
+        private int pages;
+        public int Pr4
+        {
+            get
+            {
+                return pages;
+            }
+
+            set
+            {
+               pages = value;
+            }
+        }
+
+        int IComparable.CompareTo(object obj)
+        {
+
+            Book it = (Book)obj;
+            if (this.year == it.year) return 0;
+            else if (this.year > it.year) return 1;
+            else return -1;
+
+        }
+        public void SetBook(String author, String title, int pages, int year)
+        {
+            this.author = author;
+            this.title = title;
+            this.pages = pages;
+            this.year = year;
+        }
+        public void Show()
+        {
+            Console.WriteLine("\nКнига:\n Автор: {0}\n Название: {1}\n Год издания: {2}\n {3} стр.\n", author, title, year, pages);
+        }
+      
+      
+    }
+}
---
+++ Lab07-6/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab07_6
+{
+  
+
+    class Program
+    {
+        
+        
+        static void Main(string[] args)
+        {
+
+           
+            Book[] B1 = new Book[3];
+            for (int i = 0; i < 3; i++)
+                B1[i] = new Book();
+
+            B1[0].Pr1 = "Н.В.Гоголь";
+            B1[0].Pr2 = "Мертвые души";
+            B1[0].Pr3 = 1835;
+            B1[0].Pr4 = 871;
+            B1[1].Pr1 = "А.С.Пушкин";
+            B1[1].Pr2 = "Капитанска дочка";
+            B1[1].Pr3 = 1836;
+            B1[1].Pr4 = 401;
+            B1[2].Pr1 = "М.Ю.Лермонтов";
+            B1[2].Pr2 = "Герой нашего времени";
+            B1[2].Pr3 = 1840;
+            B1[2].Pr4 = 207;
+           
+            Book B2 = new Book();
+            B2.Show();
+
+
+        }
+    }
+
+}
