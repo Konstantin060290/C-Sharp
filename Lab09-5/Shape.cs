efcc1a0f5d97c69772d1c1bad4caf0a3d3c50fdc
Konstantin060290 <73125531+Konstantin060290@users.noreply.github.com>
2020-11-08 13:19:13 +0300

    Add files via upload

---
+++ Lab09-5/Shape.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab09_5
+{
+
+    public interface IRotate
+    {
+        void Message(object o);
+    }
+
+        class Shape
+    {
+        private double a;
+        public double Pr1
+        {
+            get
+            { return a; }
+            set
+            { a = value; }
+        }
+        private double b;
+        public double Pr2
+        {
+            get
+            { return b; }
+            set
+            { b = value; }
+        }
+        private double c;
+        public double Pr3
+        {
+            get
+            { return c; }
+            set
+            { c = value; }
+        }
+        private double d;
+        public double Pr4
+        {
+            get
+            { return d; }
+            set
+            { d = value; }
+        }
+        private double p;
+        public double Pr5
+        {
+            get
+            { return p; }
+            set
+            { p = value; }
+        }
+        private double s;
+        public double Pr6
+        {
+            get
+            { return s; }
+            set
+            { s = value; }
+        }
+
+
+        public Shape(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+        public Shape(double a, double d)
+        {
+            this.a = a;
+            this.d = d;
+        }
+
+       
+    }
+}
---
+++ Lab09-5/Square.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab09_5
+{
+   
+        
+    class Square:IRotate
+    {
+       
+
+
+        Shape Sh1 = new Shape(5,0);
+        public void Initialisation()
+        {
+            //Console.WriteLine("Введите сторону квадрата:");
+            //Sh1.Pr1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Cторона квадрата равна: {0}", Sh1.Pr1);
+        }
+        public void Calc()
+        {
+            Sh1.Pr5 = 4 * Sh1.Pr1;
+            Console.WriteLine("Периметр квадрата равен:{0: 0.00}", Sh1.Pr5);
+            Sh1.Pr6 =Math.Pow(Sh1.Pr1, 2);
+            Console.WriteLine("Площадь квадрата:{0: 0.00}", Math.Round(Sh1.Pr6,2));
+            
+        }
+
+        public void Message(object o)
+        {
+            Console.WriteLine("Квадрат вращается вокург своего центра!");
+            Console.WriteLine("================================");
+        }
+
+    }
+}
